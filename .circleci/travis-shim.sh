d1ab48904e363881480e118709a07ac4455eaf1c
ProTip <greg.zapp@gmail.com>
2019-12-27 21:49:02 -0600

    Bump

---
+++ .circleci/travis-shim.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Exports environment variables and functions for use in CI environments
+set -eo pipefail
+
+export TRAVIS_BRANCH=${CIRCLE_BRANCH:-}
+export TRAVIS_TAG=${CIRCLE_TAG:-}
+export TRAVIS_BUILD_NUMBER=${CIRCLE_BUILD_NUM:-}
+export TRAVIS_COMMIT=${CIRCLE_SHA1:-}
+
+S3_ARTIFACT_BASE="s3://rundeck-ci-artifacts/oss/circle/rundeck"
